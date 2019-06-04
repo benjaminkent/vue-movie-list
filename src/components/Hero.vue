df7235eee1ae7cc3a3679d9b9f78ac5e2fa76e0e
benjaminkent <benjamin.k.jehl@gmail.com>
2019-06-03 23:04:25 -0400

    hero showing random poster

---
+++ src/components/Hero.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .hero-container
+    img.hero-image(
+      :src="`https://image.tmdb.org/t/p/w500${posters[randomNumber]}`"
+    )
+</template>
+
+<script>
+export default {
+  name: 'hero',
+  props: {
+    posters: Array,
+    randomNumber: Number
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hero-container {
+  display: flex;
+  justify-content: center;
+  padding: 50px 0;
+}
+.hero-image {
+  height: 400px;
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,41 @@
 <template lang="pug">
   .home-container
+    Hero(
+      :posters="posters"
+      :randomNumber="randomNumber"
+    )
     MovieList(:movies="movies")
 </template>
 
 <script>
 import axios from 'axios'
 import MovieList from '../components/MovieList'
+import Hero from '../components/Hero'
 
 export default {
   name: 'home',
   components: {
-    MovieList
+    MovieList,
+    Hero
   },
   data () {
     return {
-      movies: []
+      movies: [],
+      posters: [],
+      randomNumber: null
     }
   },
   mounted () {
     axios
       .get(' https://api.themoviedb.org/3/movie/now_playing?api_key=528804f087c8209bcf748a9147e6c57d&language=en-US&page=1')
-      .then(response => this.movies = response.data.results)
+      .then(response => {
+        let allPosters = response.data.results.map(movie => {
+          return movie.poster_path
+        })
+        this.movies = response.data.results,
+        this.posters = allPosters,
+        this.randomNumber = Math.floor(Math.random() * allPosters.length)
+      })
   }
 }
 </script>
